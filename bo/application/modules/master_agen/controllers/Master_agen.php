0dc6215d881b8ed764e928547073d7aef7b39fae
nurchyn <nurcahyono320@gmail.com>
2020-12-16 14:23:46 +0700

    master agen

---
+++ bo/application/modules/master_agen/controllers/Master_agen.php
@@ -0,0 +1,404 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Master_agen extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		if($this->session->userdata('logged_in') === false) {
+			return redirect('login');
+		}
+
+		$this->load->model('m_agen');
+		$this->load->model('m_user');
+		$this->load->model('m_global');
+		$this->load->model('set_role/m_set_role', 'm_role');
+	}
+
+	public function index()
+	{
+		$id_user = $this->session->userdata('id_user'); 
+		$data_user = $this->m_user->get_detail_user($id_user);
+		$data_role = $this->m_role->get_data_all(['aktif' => '1'], 'm_role');
+			
+		/**
+		 * data passing ke halaman view content
+		 */
+		$data = array(
+			'title' => 'Pengelolaan Data Master Agen',
+			'data_user' => $data_user,
+			'data_role'	=> $data_role
+		);
+
+		/**
+		 * content data untuk template
+		 * param (css : link css pada direktori assets/css_module)
+		 * param (modal : modal komponen pada modules/nama_modul/views/nama_modal)
+		 * param (js : link js pada direktori assets/js_module)
+		 */
+		$content = [
+			'css' 	=> null,
+			'modal' => 'modal_master_agen',
+			'js'	=> 'master_agen.js',
+			'view'	=> 'view_master_agen'
+		];
+
+		$this->template_view->load_view($content, $data);
+	}
+
+	public function list_agen()
+	{
+		$list = $this->m_agen->get_datatable_user();
+		$data = array();
+		$no =$_POST['start'];
+		foreach ($list as $agen) {
+			$no++;
+			$row = array();
+			//loop value tabel db
+			$row[] = $no;
+			$row[] = $agen->nama_perusahaan;
+			$row[] = $agen->produk;
+			$row[] = $agen->alamat;
+			$row[] = $agen->telp;
+			
+			$str_aksi = '
+				<div class="btn-group">
+					<button type="button" class="btn btn-sm btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Opsi</button>
+					<div class="dropdown-menu">
+						<button class="dropdown-item" onclick="edit_barang(\''.$agen->id_agen.'\')">
+							<i class="la la-pencil"></i> Edit Agen
+						</button>
+						<button class="dropdown-item" onclick="delete_barang(\''.$agen->id_agen.'\')">
+							<i class="la la-trash"></i> Hapus
+						</button>
+			';
+
+			$str_aksi .= '</div></div>';
+			$row[] = $str_aksi;
+
+			$data[] = $row;
+		}//end loop
+
+		$output = [
+			"draw" => $_POST['draw'],
+			"recordsTotal" => $this->m_agen->count_all(),
+			"recordsFiltered" => $this->m_agen->count_filtered(),
+			"data" => $data
+		];
+		
+		echo json_encode($output);
+	}
+
+	public function edit_barang()
+	{
+		$this->load->library('Enkripsi');
+		$id_user = $this->session->userdata('id_user');
+		$data_user = $this->m_user->get_by_id($id_user);
+	
+		$id = $this->input->post('id');
+		//$oldData = $this->m_user->get_by_id($id);
+
+		$select = "m_barang.*";
+		$where = ['m_barang.id_barang' => $id];
+		$table = 'm_barang';
+		// $join = [ 
+		// 	[
+		// 		'table' => 'm_role',
+		// 		'on'	=> 'm_user.id_role = m_role.id'
+		// 	]
+		// ];
+
+		$oldData = $this->m_global->single_row($select, $where, $table);
+		
+		if(!$oldData){
+			return redirect($this->uri->segment(1));
+		}
+		// var_dump($oldData);exit;
+		if($oldData->gambar) {
+			$url_foto = base_url('files/img/barang_img/').$oldData->gambar;
+		}else{
+			$url_foto = base_url('files/img/barang_img/user_default.png');
+		}
+		
+		$foto = base64_encode(file_get_contents($url_foto));  
+		
+		$data = array(
+			'data_user' => $data_user,
+			'old_data'	=> $oldData,
+			'foto_encoded' => $foto
+		);
+		
+		echo json_encode($data);
+	}
+
+	public function add_data_agen()
+	{
+		$this->load->library('Enkripsi');
+		$obj_date = new DateTime();
+		$timestamp = $obj_date->format('Y-m-d H:i:s');
+		$arr_valid = $this->rule_validasi();
+		
+		$nama_perusahaan 	= $this->input->post('nama_pers');
+		$produk 			= $this->input->post('produk');
+		$alamat				= $this->input->post('alamat');
+		$telp				= $this->input->post('telp');
+
+		if ($arr_valid['status'] == FALSE) {
+			echo json_encode($arr_valid);
+			return;
+		}
+
+		$this->db->trans_begin();
+		
+		$data_agen = [
+			'nama_perusahaan' => $nama_perusahaan,
+			'produk' => $produk,
+			'alamat' => $alamat,
+			'telp' => $telp,
+		];
+		
+		$insert = $this->m_agen->save($data_agen);
+		
+		if ($this->db->trans_status() === FALSE){
+			$this->db->trans_rollback();
+			$retval['status'] = false;
+			$retval['pesan'] = 'Gagal menambahkan master agen';
+		}else{
+			$this->db->trans_commit();
+			$retval['status'] = true;
+			$retval['pesan'] = 'Sukses menambahkan master agen';
+		}
+
+		echo json_encode($retval);
+	}
+
+	public function update_data_barang()
+	{
+		$sesi_id_user = $this->session->userdata('id_user'); 
+		$id_barang = $this->input->post('id_barang');
+		$this->load->library('Enkripsi');
+		$obj_date = new DateTime();
+		$timestamp = $obj_date->format('Y-m-d H:i:s');
+		
+		// if($this->input->post('skip_pass') != null){
+		// 	$skip_pass = true;
+		// }else{
+		// 	$skip_pass = false;
+		// }
+		
+		$arr_valid = $this->rule_validasi(true);
+
+		if ($arr_valid['status'] == FALSE) {
+			echo json_encode($arr_valid);
+			return;
+		}
+
+		$nama = $this->input->post('nama');
+		$harga = $this->input->post('harga');
+		$kategori = $this->input->post('kategori');
+		$sku      = $this->input->post('sku');
+		
+		$q = $this->m_barang->get_by_id($id_barang);
+		$namafileseo = $this->seoUrl($q->nama.' '.time());
+		
+		$this->db->trans_begin();
+
+		$file_mimes = ['image/png', 'image/x-citrix-png', 'image/x-png', 'image/x-citrix-jpeg', 'image/jpeg', 'image/pjpeg'];
+
+		if(isset($_FILES['foto']['name']) && in_array($_FILES['foto']['type'], $file_mimes)) {
+			$this->konfigurasi_upload_img($namafileseo);
+			//get detail extension
+			$pathDet = $_FILES['foto']['name'];
+			$extDet = pathinfo($pathDet, PATHINFO_EXTENSION);
+			
+			if ($this->file_obj->do_upload('foto')) 
+			{
+				$gbrBukti = $this->file_obj->data();
+				$nama_file_foto = $gbrBukti['file_name'];
+				$this->konfigurasi_image_resize($nama_file_foto);
+				$output_thumb = $this->konfigurasi_image_thumb($nama_file_foto, $gbrBukti);
+				$this->image_lib->clear();
+				## replace nama file + ext
+				$namafileseo = $this->seoUrl($q->nama.' '.time()).'.'.$extDet;
+				$foto = $namafileseo;
+			} else {
+				$error = array('error' => $this->file_obj->display_errors());
+				var_dump($error);exit;
+			}
+		}else{
+			$foto = null;
+		}
+
+		$data_barang = [
+			'nama' => $nama,
+			'sku' => $sku,
+			'harga' => $harga,
+			'id_kategori' => $kategori,
+			'updated_at' => $timestamp
+		];
+		
+		if($foto != null) {
+			$data_barang['gambar'] = $foto;
+		}
+
+		$where = ['id_barang' => $id_barang];
+		$update = $this->m_barang->update($where, $data_barang);
+
+		if ($this->db->trans_status() === FALSE){
+			$this->db->trans_rollback();
+			$data['status'] = false;
+			$data['pesan'] = 'Gagal update Master Barang';
+		}else{
+			$this->db->trans_commit();
+			$data['status'] = true;
+			$data['pesan'] = 'Sukses update Master Barang';
+		}
+		
+		echo json_encode($data);
+	}
+
+	/**
+	 * Hanya melakukan softdelete saja
+	 * isi kolom updated_at dengan datetime now()
+	 */
+	public function delete_barang()
+	{
+		$id_barang = $this->input->post('id');
+		$del = $this->m_barang->softdelete_by_id($id_barang);
+		if($del) {
+			$retval['status'] = TRUE;
+			$retval['pesan'] = 'Data Master Barang berhasil dihapus';
+		}else{
+			$retval['status'] = FALSE;
+			$retval['pesan'] = 'Data Master Barang berhasil dihapus';
+		}
+
+		echo json_encode($retval);
+	}
+
+	public function edit_status_user($id)
+	{
+		$input_status = $this->input->post('status');
+		// jika aktif maka di set ke nonaktif / "0"
+		$status = ($input_status == "aktif") ? $status = 0 : $status = 1;
+			
+		$input = array('status' => $status);
+
+		$where = ['id' => $id];
+
+		$this->m_user->update($where, $input);
+
+		if ($this->db->affected_rows() == '1') {
+			$data = array(
+				'status' => TRUE,
+				'pesan' => "Status User berhasil di ubah.",
+			);
+		}else{
+			$data = array(
+				'status' => FALSE
+			);
+		}
+
+		echo json_encode($data);
+	}
+
+	// ===============================================
+	private function rule_validasi($is_update=false, $skip_pass=false)
+	{
+		$data = array();
+		$data['error_string'] = array();
+		$data['inputerror'] = array();
+		$data['status'] = TRUE;
+
+		
+		
+		// if ($this->input->post('icon_menu') == '') {
+		// 	$data['inputerror'][] = 'icon_menu';
+        //     $data['error_string'][] = 'Wajib mengisi icon menu';
+        //     $data['status'] = FALSE;
+		// }
+
+		if ($this->input->post('nama_pers') == '') {
+			$data['inputerror'][] = 'nama_pers';
+            $data['error_string'][] = 'Wajib Mengisi Nama Perusahaan';
+            $data['status'] = FALSE;
+		}
+
+		if ($this->input->post('produk') == '') {
+			$data['inputerror'][] = 'produk';
+            $data['error_string'][] = 'Wajib Mengisi Produk';
+            $data['status'] = FALSE;
+		}
+
+		if ($this->input->post('alamat') == '') {
+			$data['inputerror'][] = 'alamat';
+            $data['error_string'][] = 'Wajib Mengisi Alamat';
+            $data['status'] = FALSE;
+		}
+
+        return $data;
+	}
+
+	private function konfigurasi_upload_img($nmfile)
+	{ 
+		//konfigurasi upload img display
+		$config['upload_path'] = './files/img/barang_img/';
+		$config['allowed_types'] = 'gif|jpg|png|jpeg|bmp';
+		$config['overwrite'] = TRUE;
+		$config['max_size'] = '4000';//in KB (4MB)
+		$config['max_width']  = '0';//zero for no limit 
+		$config['max_height']  = '0';//zero for no limit
+		$config['file_name'] = $nmfile;
+		//load library with custom object name alias
+		$this->load->library('upload', $config, 'file_obj');
+		$this->file_obj->initialize($config);
+	}
+
+	private function konfigurasi_image_resize($filename)
+	{
+		//konfigurasi image lib
+	    $config['image_library'] = 'gd2';
+	    $config['source_image'] = './files/img/user_img/'.$filename;
+	    $config['create_thumb'] = FALSE;
+	    $config['maintain_ratio'] = FALSE;
+	    $config['new_image'] = './files/img/user_img/'.$filename;
+	    $config['overwrite'] = TRUE;
+	    $config['width'] = 450; //resize
+	    $config['height'] = 500; //resize
+	    $this->load->library('image_lib',$config); //load image library
+	    $this->image_lib->initialize($config);
+	    $this->image_lib->resize();
+	}
+
+	private function konfigurasi_image_thumb($filename, $gbr)
+	{
+		//konfigurasi image lib
+	    $config2['image_library'] = 'gd2';
+	    $config2['source_image'] = './files/img/user_img/'.$filename;
+	    $config2['create_thumb'] = TRUE;
+	 	$config2['thumb_marker'] = '_thumb';
+	    $config2['maintain_ratio'] = FALSE;
+	    $config2['new_image'] = './files/img/user_img/thumbs/'.$filename;
+	    $config2['overwrite'] = TRUE;
+	    $config2['quality'] = '60%';
+	 	$config2['width'] = 45;
+	 	$config2['height'] = 45;
+	    $this->load->library('image_lib',$config2); //load image library
+	    $this->image_lib->initialize($config2);
+	    $this->image_lib->resize();
+	    return $output_thumb = $gbr['raw_name'].'_thumb'.$gbr['file_ext'];	
+	}
+
+	private function seoUrl($string) {
+	    //Lower case everything
+	    $string = strtolower($string);
+	    //Make alphanumeric (removes all other characters)
+	    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+	    //Clean up multiple dashes or whitespaces
+	    $string = preg_replace("/[\s-]+/", " ", $string);
+	    //Convert whitespaces and underscore to dash
+	    $string = preg_replace("/[\s_]/", "-", $string);
+	    return $string;
+	}
+}
