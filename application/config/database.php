a8fe248cb3dfb47bc4e476685ad9088976405632
Dmenk123 <rizkiyuandaa@gmail.com>
2021-02-10 18:56:26 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -6,7 +6,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhosta',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'sparepart',
