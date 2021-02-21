91416c7b328d67b799d8bd51b35bf62c5ef73d1f
nurchyn <nurcahyono320@gmail.com>
2021-02-21 14:52:12 +0700

    sajdb

--- application/views/temp_component/v_produk_kategori.php
+++ application/views/temp_component/v_produk_kategori.php
@@ -39,22 +39,41 @@
                     <div class="categories_product_area">
                         <div class="row" id="produk_area">
                             <?php foreach ($results as $key => $value) { ?>
-                            <div class="col-lg-4 col-sm-6">
-                                <div class="l_product_item">
-                                    <div class="l_p_img">
-                                        <img src="<?php echo base_url();?>bo/files/img/barang_img/resize_image/<?= $value->gambar;?>" alt="">
-                                        <!-- <h5 class="new"><?= $value->nama;?></h5> -->
-                                    </div>
-                                    <div class="l_p_text">
-                                        <ul>
-                                            <li><a class="add_cart_btn" href="<?= base_url('produk/detail/'.seourl($value->nama_kategori).'/'.seourl($value->nama)); ?>">Lihat Detail</a></li>
-                                        </ul>
-                                        <h4><?= $value->nama;?></h4>
-                                        <!-- <h5><del>$45.50</del>  $40</h5> -->
-                                        <h5><?= "Rp " . number_format($value->harga,0,',','.');?></h5>
+        
+                                <div class="col-sm-3 col-sm-3 item-pc">
+                                    <div class="l_product_item">
+                                        <div class="l_p_img">
+                                            <img src="<?php echo base_url();?>bo/files/img/barang_img/resize_image/<?= $value->gambar;?>" alt="">
+                                            <!-- <h5 class="new"><?= $value->nama;?></h5> -->
+                                        </div>
+                                        <div class="l_p_text">
+                                            <ul>
+                                                <li><a class="add_cart_btn" href="<?= base_url('produk/detail/'.seourl($value->nama_kategori).'/'.seourl($value->nama)); ?>">Lihat Detail</a></li>
+                                            </ul>
+                                            <h4><?= $value->nama;?></h4>
+                                            <!-- <h5><del>$45.50</del>  $40</h5> -->
+                                            <h5><?= "Rp " . number_format($value->harga,0,',','.');?></h5>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+
+                                <!-- <div class="col-sm-3 col-sm-3 item-hp" style="display: none;"> -->
+                                    <div class="l_product_item item-hp" style="display: none;">
+                                        <div class="l_p_img">
+                                            <img src="<?php echo base_url();?>bo/files/img/barang_img/resize_image/<?= $value->gambar;?>" alt="">
+                                            <!-- <h5 class="new"><?= $value->nama;?></h5> -->
+                                        </div>
+                                        <div class="l_p_text">
+                                            <ul>
+                                                <li><a class="add_cart_btn" href="<?= base_url('produk/detail/'.seourl($value->nama_kategori).'/'.seourl($value->nama)); ?>">Lihat Detail</a></li>
+                                            </ul>
+                                            <h4><?= $value->nama;?></h4>
+                                            <!-- <h5><del>$45.50</del>  $40</h5> -->
+                                            <h5><?= "Rp " . number_format($value->harga,0,',','.');?></h5>
+                                        </div>
+                                    </div>
+                                <!-- </div> -->
+                   
                             <?php } ?>
                         </div>
 
