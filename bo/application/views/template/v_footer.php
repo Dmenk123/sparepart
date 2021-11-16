85a28683cbf035016e493d368580aace4475741c
Dmenk123 <rizkiyuandaa@gmail.com>
2021-11-16 16:55:29 +0700

    asasasa

--- bo/application/views/template/v_footer.php
+++ bo/application/views/template/v_footer.php
@@ -17,11 +17,6 @@ echo $modal_excel_upload;
         <div class="kt-footer__copyright">
             2021&nbsp;&copy;&nbsp;Melek Aplikasi
         </div>
-        <!-- <div class="kt-footer__menu">
-            <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">About</a>
-            <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
-            <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
-        </div> -->
     </div>
 </div>
 <!-- end:: Footer -->
