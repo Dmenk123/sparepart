8a514de57766ea16a693925d95e92a9feaca93ba
Dmenk123 <rizkiyuandaa@gmail.com>
2021-11-16 11:56:39 +0700

    aiwiw

---
+++ bo/Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = (grunt) => {
+    /**
+    * Project configuration.
+    */
+    grunt.registerTask('code-minify', 'Minify CSS code and JS code.', () => {
+
+        /**
+        * Load NPM tasks.
+        */
+        grunt.loadNpmTasks('grunt-contrib-cssmin')
+        grunt.loadNpmTasks('grunt-contrib-uglify')
+
+        /**
+        * Create configuration.
+        */
+        grunt.initConfig({
+
+            /**
+            * Minify CSS code.
+            */
+            cssmin: {
+                options: {
+                    level: { 1: { specialComments:0 } }
+                },
+                main: {
+                    files: [{
+                        expand:true,
+                        cwd:'/assets/build/css',
+                        src:'style.css',
+                        dest:'_dist/css',
+                        ext:'.css'
+                    }]
+                }
+            },
+
+            /**
+            * Minify JS code.
+            */
+            uglify: {
+                main: {
+                    files: [{
+                        expand:true,
+                        cwd:'assets/js_module',
+                        src:['*.js'],
+                        dest:'build/js',
+                        ext:'.min.js'
+                    }]
+                }
+            }
+        })
+
+        /**
+        * Run tasks.
+        */
+        grunt.task.run(['cssmin', 'uglify'])
+    })
+}
