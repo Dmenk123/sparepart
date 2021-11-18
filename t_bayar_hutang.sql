16c783c7a3d1b1f44ed510357b06cbf8b36b9cea
Dmenk123 <rizkiyuandaa@gmail.com>
2021-11-19 01:56:54 +0700

    wwkwk

--- t_bayar_hutang.sql
+++ t_bayar_hutang.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : local-mysql
  Source Server Type    : MySQL
- Source Server Version : 50731
+ Source Server Version : 100413
  Source Host           : localhost:3306
  Source Schema         : sparepart
 
  Target Server Type    : MySQL
- Target Server Version : 50731
+ Target Server Version : 100413
  File Encoding         : 65001
 
- Date: 18/11/2021 16:13:35
+ Date: 19/11/2021 01:30:06
 */
 
 SET NAMES utf8mb4;
@@ -22,16 +22,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_bayar_hutang`;
 CREATE TABLE `t_bayar_hutang`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
+  `id_bayar_det` int(11) NOT NULL,
   `id_pembelian` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `total_bayar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `kode` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `id_user` int(11) NULL DEFAULT NULL,
   `tanggal` date NULL DEFAULT NULL,
   `created_at` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `updated_at` datetime NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
   `deleted_at` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `kode` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+  `keterangan` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `nilai_bayar` float(20, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `id_bayar_det`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of t_bayar_hutang
